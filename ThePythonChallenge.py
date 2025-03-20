93197e512e63811453a8fb49c22dcc48d776d297
Zachary Vance <za3k@za3k.com>
2025-03-20 13:57:04 -0400

    Completed lvl 3

--- ThePythonChallenge.py
+++ ThePythonChallenge.py
@@ -16,7 +16,7 @@ def lvl_1():
     """Solve the lvl 1 puzzle (at http://www.pythonchallenge.com/pc/def/map.html) and print the url to lvl 2"""
     cryptic_string = read_file("lvl1.txt")
     new_string = shift_string(cryptic_string,2)
-    print(f'lvl 1 decoded hint: {new_string}')
+    #print(f'lvl 1 decoded hint: {new_string}')
     # Hint says to shift the URL!
     # The original url is http://www.pythonchallenge.com/pc/def/map.html
     # The correct part to shift is just                         ^^^
@@ -52,19 +52,7 @@ def number_to_letter(number : int) -> Optional[str]:
             return letters[i]
 
 def is_shiftable_letter(letter : str) -> bool:
-    '''
-    Returns True if input is a lowercase english letter. Otherwise, returns False.
-    '''
-    #return letter in 'abcdefghijklmnopqrstuvwxyz'
-    #if letter in 'abcdefghijklmnopqrstuvwxyz':
-    #    return True
-    #else:
-    #    return False
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    for candidate in letters:
-        if letter == candidate:
-            return True
-    return False
+    return is_lower(letter)
 
 def shift_string(unshifted_text : str, shift_forward : int) -> str:
     '''
@@ -84,17 +72,58 @@ def shift_string(unshifted_text : str, shift_forward : int) -> str:
 def lvl_2():
     lvl2_text = read_file('lvl2.txt')
     characters = set(lvl2_text)
-    print((characters))
-    for i in characters:
-        if is_shiftable_letter(i):
-            print(i)
-    return characters
+    #print((characters))
+    #for i in characters:
+        #if is_shiftable_letter(i):
+            #print(i)
+    print('lvl 2 solution: http://www.pythonchallenge.com/pc/def/equality.html')
+
+def lvl_3():
+    lvl3_text = read_file('lvl3.txt')
+    ans3 = ''
+    for index in range(4, len(lvl3_text)-4):
+        left_character = lvl3_text[index - 1]
+        right_character = lvl3_text[index + 1]
+        left_left_character = lvl3_text[index - 2]
+        right_right_character = lvl3_text[index + 2]
+        left_left_left_character = lvl3_text[index - 3]
+        right_right_right_character = lvl3_text[index + 3]
+        left_left_left_left_character = lvl3_text[index - 4]
+        right_right_right_right_character = lvl3_text[index + 4]
+        character = lvl3_text[index]
+        if (character.islower()
+            and left_character.isupper()
+            and right_character.isupper()
+            and left_left_character.isupper()
+            and right_right_character.isupper()
+            and left_left_left_character.isupper()
+            and right_right_right_character.isupper()
+            and left_left_left_left_character.islower()
+            and right_right_right_right_character.islower()):
+            ans3 += character
+    print(f'lvl 3 solution: http://www.pythonchallenge.com/pc/def/{ans3}.php')
+
+def is_upper(character: str) -> bool:
+    '''
+    Checks if a letter is uppercase: returns True if yes, returns False if no.
+    '''
+    return character in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def is_lower(character: str) -> bool:
+    '''
+    Checks if a letter is lowercase: returns True if yes, returns False if no.
+    '''
+    return character in 'abcdefghijklmnopqrstuvwxyz'
+
+
 
 
 def main():
     lvl_0()
     lvl_1()
     lvl_2()
+    lvl_3()
+
     # print(letter_to_number('z'))
     # print(number_to_letter(25))
     # print(shift_letter('a',2))
